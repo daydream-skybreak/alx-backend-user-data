7735c7864e0127fc384b52ae606e7d24a080313d
Samuel <samueldesta323@gmail.com>
2023-03-07 14:05:20 +0300

    added task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """basic authentication module"""
+import base64
+import binascii
 import re
 
 from api.v1.auth.auth import Auth
@@ -17,3 +19,14 @@ class BasicAuth(Auth):
             return None
         if authorization_header.split()[0] == "Basic":
             return authorization_header.split()[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """returns decoded value of base64 string"""
+        if type(base64_authorization_header) == str:
+            try:
+                response = base64.b64decode(base64_authorization_header,
+                                            validate=True)
+                return response.decode('utf-8')
+            except (binascii.Error, UnicodeDecodeError):
+                return None
