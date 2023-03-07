ca387c402305869796863af0644034f0196c361e
Samuel <samueldesta323@gmail.com>
2023-03-07 13:49:47 +0300

    added task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 """basic authentication module"""
+import re
+
 from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
     """handles basic authorization"""
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """extracts base64 authorization header"""
+        if authorization_header is None or\
+                not isinstance(authorization_header, str):
+            return None
+        if len(authorization_header.split()) < 2:
+            return None
+        if authorization_header.split()[0] == "Basic":
+            return authorization_header.split()[1]
