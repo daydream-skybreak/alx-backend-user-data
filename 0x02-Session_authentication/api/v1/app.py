4d2c3f429a328376b2f9eae3cc18891243979371
Samuel <samueldesta323@gmail.com>
2023-03-09 22:06:04 +0300

    fixed authentication bug

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -34,11 +34,11 @@ def b4_request():
                       '/api/v1/auth_session/login/']
     if auth:
         if auth.require_auth(request.path, forbidden_list):
-            if auth.current_user(request) is None:
-                abort(403)
             if auth.authorization_header(request) is None and\
                     auth.session_cookie(request) is None:
                 abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
             request.current_user = auth.current_user(request)
 
 
