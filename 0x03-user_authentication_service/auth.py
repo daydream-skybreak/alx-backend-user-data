0e2f7b8052f6cd3a08c29982b21a3232c23088ea
Samuel <samueldesta323@gmail.com>
2023-03-15 21:37:31 +0300

    added task 4

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""auth"""
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """returns a salted hash of the input password"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
