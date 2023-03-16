183f73a8e5543a296c7172b244e6aeecbd440679
Samuel <samueldesta323@gmail.com>
2023-03-16 23:51:28 +0300

    task 17 done

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -64,5 +64,17 @@ def profile():
     return jsonify({"email": user.email})
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def reset_password():
+    """resets password"""
+    email = request.form.get('email')
+    reset_token = None
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": email, "reset_token": reset_token})
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
