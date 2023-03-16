34267e657cd93559a0071801e60bb1ef01810643
Samuel <samueldesta323@gmail.com>
2023-03-16 11:07:49 +0300

    added task auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,7 @@
 from db import DB
 import bcrypt
 from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -19,6 +20,8 @@ class Auth:
 
     def register_user(self, email: str, password: str) -> User:
         """register the user to the database"""
-        if self._db.find_user_by(email=email):
-            raise ValueError(f'User {email} already exists')
-        return self._db.add_user(email, _hash_password(password))
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+        raise ValueError(f'User {email} already exists')
